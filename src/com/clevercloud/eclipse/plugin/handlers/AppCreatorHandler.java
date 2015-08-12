9fa9e33218f1d4f8c4f328050bf46e7d5a0ccf56
DROUARD Benjamin <drouar_b@epitech.eu>
2015-08-12 17:59:33 +0200

    Creating handlers for app creation and import

---
+++ src/com/clevercloud/eclipse/plugin/handlers/AppCreatorHandler.java
@@ -0,0 +1,18 @@
+package com.clevercloud.eclipse.plugin.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class AppCreatorHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		return null;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return false;
+	}
+}
---
+++ src/com/clevercloud/eclipse/plugin/handlers/AppLinkerHandler.java
@@ -0,0 +1,14 @@
+package com.clevercloud.eclipse.plugin.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class AppLinkerHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		return null;
+	}
+
+}
